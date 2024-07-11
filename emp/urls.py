5d6b5fe3112aaca499f7c75c8c061e085eb3ee44
muskan155 <muskanrauniyar82@gmail.com>
2024-07-11 14:41:34 +0545

    Executed database migrations and set up SQLite viewer in vs code.

--- emp/urls.py
+++ emp/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index),
-    path('about',views.about),
-    path('contact',views.contact),
-    path('news',views.news),
-    path('students/<id>',views.students),
+    path('',views.index,name='index'),
+    path('about',views.about,name='about'),
+    path('contact',views.contact,name='contact'),
+    path('news',views.news,name='news'),
+    path('students/<id>',views.students,name='students'),
 ]
